4a7901fc7fdf9c7f4b41f3a31c07866c0d3e47d3
endritxh <endritxhemaili91@gmail.com>
2022-11-03 12:40:00 +0100

    scripta

--- Assets/SkriptaTest.cs
+++ Assets/SkriptaTest.cs
@@ -13,6 +13,19 @@ public class SkriptaTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(true)
+        {
+            ///
+        }
+
+        //for (int i = 0; i < length; i++)
+        //{
+
+        //}
+
+        //foreach (var item in collection)
+        //{
+
+        //}
     }
 }
